b63aac31d6f53e41b39d6d1687c8d71fe8e31905
KSGAMER <ksgamer03mail.com>
2018-10-14 19:56:58 -0500

    Actualizacion

--- src/app/services/recipe.service.ts
+++ src/app/services/recipe.service.ts
@@ -1,15 +1,16 @@
 import {Recipe} from '../recipes/recipe.model';
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 export class RecipeService{
     recipeSelected = new EventEmitter<Recipe>();
 
     recipes: Recipe[] = [
         new Recipe('A test recipe 1', ' This is a simply test',
-        'https://www.milkbusiness.com/sites/default/files/2018-02/Pizza%20Hut%20Pan%20Pizza.jpg'),
+        'https://www.milkbusiness.com/sites/default/files/2018-02/Pizza%20Hut%20Pan%20Pizza.jpg', [new Ingredient('Bread', 2)]),
         new Recipe('A test recipe 2', ' This is a simply test',
-        'https://www.milkbusiness.com/sites/default/files/2018-02/Pizza%20Hut%20Pan%20Pizza.jpg'),
+        'https://www.milkbusiness.com/sites/default/files/2018-02/Pizza%20Hut%20Pan%20Pizza.jpg', [new Ingredient('Apple', 2)]),
         new Recipe('A test recipe 3', ' This is a simply test',
-        'https://www.milkbusiness.com/sites/default/files/2018-02/Pizza%20Hut%20Pan%20Pizza.jpg')
+        'https://www.milkbusiness.com/sites/default/files/2018-02/Pizza%20Hut%20Pan%20Pizza.jpg', [new Ingredient('Juice', 2), new Ingredient('Orange', 3)])
       ];
 
       getRecipes(){
